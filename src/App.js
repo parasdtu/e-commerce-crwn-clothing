2e9d16afbdcf3826c6bf3026ec639360fac11481
PARAS <helloiamparas@gmail.com>
2020-03-25 08:20:19 +0530

    added link for hats shopping page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-     <HomePage />
+    <div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
