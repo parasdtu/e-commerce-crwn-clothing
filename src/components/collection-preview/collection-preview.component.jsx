0705c5ffddc6f6cfbf5ce6f832d4ff023252f013
PARAS <helloiamparas@gmail.com>
2020-03-30 20:22:20 +0530

    added individual items page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './components/redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import {PersistGate}  from 'redux-persist/integration/react';
+import {store, persistor} from './components/redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
+  <PersistGate persistor={persistor}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </PersistGate>
   </BrowserRouter>
   </Provider>
   ,
